c71d84993af37949468b9ef7b0078a6c261737c7
julian <jcristancho838@gmail.com>
2024-10-20 22:47:59 -0500

    se añade el codigo y su complejidad

---
+++ src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.figurasgeometricas;
+
+public class Triangulo extends FiguraGeometrica {
+
+    private double lado;
+
+    // Complejidad: O(1)
+    public Triangulo(String nombre, String color, double lado) {
+        super(nombre, color);
+        this.lado = lado;
+    }
+
+    @Override
+    public double obtenerArea() {
+        return 0.433 * lado * lado;
+    }
+
+    // Complejidad: O(1)
+    @Override
+    public double obtenerPerimetro() {
+        return 3 * lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    // Complejidad: O(1)
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+}
